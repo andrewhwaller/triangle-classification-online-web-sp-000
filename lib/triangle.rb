e76ca395b5fd4a71c126af04badb34aab9e1c81d
Andrew Waller <andrewhwaller@gmail.com>
2019-03-23 03:46:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  # write code here
+  attr_reader :length_1, :length_2, :length_3
+
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    :type
+  end
 end
